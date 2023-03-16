8da7806f5bfa98166d7b3bb98bbf547a23f84a01
Richard Achmatowicz <rachmato@redhat.com>
2023-03-16 18:32:10 -0400

    Add a second test.

---
+++ src/main/test/java/org/example/MainTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void testMethod() {
+        System.out.println("This is a test");
+    }
+
+    @Test
+    public void testMethod2() {
+        System.out.println("This is a second test");
+    }
+
+}
