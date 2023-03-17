2b44e60421f435ff49632843a0898c73b1ff22a3
Richard Achmatowicz <rachmato@redhat.com>
2023-03-17 07:46:07 -0400

    Modify tests

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -11,12 +11,14 @@ public class MainTest {
     @Test
     public void testMethod2() {
         System.out.println("This is a second test, with properties");
+        // print systenm properties
         System.out.println(System.getProperties()) ;
     }
 
     @Test
     public void testMethod3() {
         System.out.println("This is a third test, with environment");
+        // print environment
         System.out.println(System.getenv());
     }
 
